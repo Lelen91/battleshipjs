eec70b1d46b41ffb8bbf730164c5dc7bdbcaef94
Raffaele Nicosia <you@example.com>
2020-03-01 20:57:34 +0000

    add new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import Join from './components/Join';
-import Room from './components/Room.js';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
   <Router>
     <Route path="/" exact component={Join} />
-    <Route path="/room" component={Room} />
+    <Route path="/chat" component={Chat} />
   </Router>
 );
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
 const socketIo = require('socket.io');
-const axios = require('axios');
+// const axios = require('axios');
+
+const { addUser, removeUser, getUser, getRoomUsers } = require('./users/index'); 
 
 dotenv.config({path: './config/config.env'})
 
@@ -18,7 +20,37 @@ server.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV}
 
 // Socket setup
 const io = socketIo(server);
+
+//io.on() wait for an event within the front end
 io.on('connection', socket => {
-    console.log('New client connected');
-    socket.on("disconnect", () => console.log("Client disconnected"));
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        //emit an event to the front end
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}.` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+
+        socket.join(user.room);
+
+        //Ensure the callback function in the front end gets called every time, the if statement in the front end won't be executed if there's no error
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => { 
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('message', { user:'admin', text: `${user.name} has left.`})
+        }
+    });
 });
